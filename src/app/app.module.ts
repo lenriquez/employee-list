4bd21ba4d9e0d97be6081e8234823859ee4735c6
Luis Felipe <felipe096@gmail.com>
2018-09-18 20:11:05 -0500

    Added file structure and router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { routing } from './routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeeListComponent } from './employee/containers/employee-list/employee-list.component';
+import { EmployeeEditComponent } from './employee/containers/employee-edit/employee-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    EmployeeEditComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
