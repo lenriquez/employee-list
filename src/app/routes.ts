bc0fedc7df5c1797b3449798c2fc156d5a799b21
Luis Felipe <felipe096@gmail.com>
2018-09-18 20:18:37 -0500

    Added routes to main containers

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
+import { EmployeeListComponent } from './employee/containers/employee-list/employee-list.component';
+import { EmployeeEditComponent } from './employee/containers/employee-edit/employee-edit.component';
 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/employees', pathMatch: 'full' },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'employee/new_employee', component: EmployeeEditComponent },
+  { path: 'employee/:id', component: EmployeeEditComponent },
+];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
